07cd68b75cef0949864d1d39baec9d031adbd083
mohammadtuhin0 <md.tuhin42525@gmail.com>
2025-11-07 08:37:12 +0600

    Basic Python code

--- Variables & Data Types/sum.py
+++ Variables & Data Types/sum.py
@@ -2,3 +2,19 @@ a = 12
 b = 8
 sum = a + b
 print(sum)
+
+# deff
+diff = a - b
+print(diff)
+
+# multi
+multi = a * b
+print(multi)
+
+# divided
+divid = a / b
+print(divid)
+
+# Modul
+modul = a % b
+print(modul)
