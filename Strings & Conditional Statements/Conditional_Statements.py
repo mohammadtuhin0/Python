dd247bdd4864e54f712ec1f0efbd2cace69f6e91
mohammadtuhin0 <md.tuhin42525@gmail.com>
2025-12-03 21:34:19 +0600

    basic code

---
+++ Strings & Conditional Statements/Conditional_Statements.py
@@ -0,0 +1,13 @@
+# if-elif-else(SYNTAX)
+
+light = "green"
+if(light == "red"):
+    print("Stop")
+elif(light == "green") :
+    print("Go")
+elif(light == "yellow"):
+    print("look")
+else:
+    print("Light is broken")
+    
+print("End of code")
