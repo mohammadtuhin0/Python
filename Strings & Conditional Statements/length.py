d5d02ac303a4a98dd5b207925c3e3dfd8f564806
mohammadtuhin0 <md.tuhin42525@gmail.com>
2025-12-03 21:23:30 +0600

    2 practice problem

---
+++ Strings & Conditional Statements/length.py
@@ -0,0 +1,10 @@
+# write a program to input user's first name & print its length
+
+name = input("What is your name : ")
+print("Length of your name : ", len(name))
+
+
+# WAP to find the occurrence of '$' in a string
+
+str = "Hi, $Iam the $ symbol $99.00"
+print("Count $ : ", str.count("$"))
