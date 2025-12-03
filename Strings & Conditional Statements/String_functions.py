7c04dd80d7fc1fb83e352f31389795d275f3f163
mohammadtuhin0 <md.tuhin42525@gmail.com>
2025-12-03 21:17:36 +0600

    basic code

---
+++ Strings & Conditional Statements/String_functions.py
@@ -0,0 +1,13 @@
+str = "i am studying python from Apna College"
+
+print(str.endswith("ege"))
+
+print(str.capitalize())
+
+print(str.replace("python", "Cpp"))
+
+print(str.find("o"))
+
+print(str.count("from"))
+
+print(str)      # not change in main function
