47de33956c3b1f264d869a84e52d3cb20e64ac94
mohammadtuhin0 <md.tuhin42525@gmail.com>
2026-01-05 19:51:24 +0600

    solve

---
+++ List & Tuple/practice.py
@@ -0,0 +1,11 @@
+# write a program to ask the user enter names of their 3 favorite movies & store them in a list.
+
+movies = []
+mov = input("Enter 1st moive: ")
+movies.append(mov)
+mov = input("Enter 2nd moive: ")
+movies.append(mov)
+mov = input("Enter 3rd moive: ")
+movies.append(mov)
+
+print(movies)
