bbb3c2dad12fe50f7656299f09d81e863f867a95
mohammadtuhin0 <md.tuhin42525@gmail.com>
2026-01-05 19:16:41 +0600

    basic code

---
+++ List & Tuple/list_slicing.py
@@ -0,0 +1,10 @@
+marks = [85, 94, 76, 63, 48]
+# print(marks[:4])
+print(marks[-2])
+
+list = [2, 1, 3, 4]
+list.append(5)
+list.sort()
+list.sort(reverse=True)
+list.insert(1,10)       # : index,change
+print(list)
