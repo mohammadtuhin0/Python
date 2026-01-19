5a7452a8d83f41b33aab3e2135d07976b5a788c7
mohammadtuhin0 <md.tuhin42525@gmail.com>
2026-01-19 19:12:17 +0600

    basic set

---
+++ Dictionary & Set/set_in.py
@@ -0,0 +1,10 @@
+# Set is the collection of the unordered items.
+
+collection = {1, 2, 3, 4, "hello", "world"}
+
+print(collection)
+print(type(collection))
+
+collection1 = set ()        #empty set.
+print(type(collection1))
+
