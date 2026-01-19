682ccd8933974d58f3ca707b191caabe5560faab
mohammadtuhin0 <md.tuhin42525@gmail.com>
2026-01-19 19:34:25 +0600

    practice

---
+++ Dictionary & Set/practice.py
@@ -0,0 +1,15 @@
+# Dictionary
+dictionary = {
+    "cat" : "a small animal",
+    "table" : ["a piece of furniture", "list of facts & figures"]
+}
+
+print(dictionary)
+
+# Set 
+subjects = {
+    "python", "java", "c++", "python", "javascript", "java",
+    "python", "java", "c++", "c"
+}
+
+print(len(subjects))
