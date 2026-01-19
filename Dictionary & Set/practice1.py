d3b3adc6fc5447d6aaceae2785e800098425edde
mohammadtuhin0 <md.tuhin42525@gmail.com>
2026-01-19 19:45:52 +0600

    basic practice

---
+++ Dictionary & Set/practice1.py
@@ -0,0 +1,21 @@
+# Dict 
+marks = {}
+
+x = int(input("enter phy : "))
+marks.update({"phy" : x})
+
+x = int(input("enter math : "))
+marks.update({"math" : x})
+
+x = int(input("enter chem : "))
+marks.update({"chem" : x})
+
+print(marks)
+
+
+# set 
+values = {
+    ("float", 9.0),
+    ("int", 9)
+}
+print(values)
