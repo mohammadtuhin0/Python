af3a97e811f454d04c8f41b14db0610044bad63d
mohammadtuhin0 <md.tuhin42525@gmail.com>
2026-01-29 20:49:50 +0600

    practice

---
+++ Loops in Python/whileLoop.py
@@ -0,0 +1,14 @@
+nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+idx = 0
+while idx < len(nums):
+    print(nums[idx])
+    idx += 1
+    
+    
+heros = ["ironman", "thor", "superman", "batman"]
+
+index = 0
+while index <len(heros):
+    print(heros[index])
+    index += 1
