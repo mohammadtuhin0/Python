6d8d3c66fa40382b40e91254e5be1267885779e5
mohammadtuhin0 <md.tuhin42525@gmail.com>
2026-01-29 20:28:19 +0600

    basic while loop

---
+++ Loops in Python/while.py
@@ -0,0 +1,24 @@
+print("hello")
+
+
+count = 1
+while count <= 5 :
+    print("hello world!")
+    count += 1
+    
+i = 1
+while i <= 5:
+    print("Mohammad Tuhin", i)
+    i += 1
+    
+# number print in loop 
+print("Number print in loop : ")
+x = 1
+while x <= 10:
+    print(x)
+    x += 1
+    
+y = 10
+while y >= 1:
+    print(y)
+    y -= 1
