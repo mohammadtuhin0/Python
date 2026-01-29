c79344b38dcddd6266c1f1f50e773cc95f519600
mohammadtuhin0 <md.tuhin42525@gmail.com>
2026-01-29 21:32:43 +0600

    range

---
+++ Loops in Python/range.py
@@ -0,0 +1,14 @@
+# range()
+# Range functions returns a sequence of numbers, starting from 0 by default, and increment by 1(by default), and stops before a specified number. 
+# range(start?, stop, step?) 
+
+for i in range(10):         #range(stop)
+    print(i)
+    
+
+for i in range(1, 10):      #range(start, stop)
+    print(i)
+    
+    
+for i in range(1, 10, 2):   #range(start, stop, step)
+    print(i)
