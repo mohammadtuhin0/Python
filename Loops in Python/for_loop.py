16e3716343fe1aa70398b6d9218a47e30a49ad0e
mohammadtuhin0 <md.tuhin42525@gmail.com>
2026-01-29 21:15:00 +0600

    basic for loop

---
+++ Loops in Python/for_loop.py
@@ -0,0 +1,15 @@
+nums = [1,2,3,4,5]
+
+for val in nums:
+    print(val)
+    
+veggies = ["potato", "brijal", "ladyfinger", "cucumber"]
+for val in veggies:
+    print(val)
+    
+    
+# tuple 
+print("Tuple in loop :")
+tup = (1,2,3,4,5,3,2)
+for num in tup:
+    print(num)
