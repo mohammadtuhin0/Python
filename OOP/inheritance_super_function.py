12e3085cac9f4a72e2966623bbac33fdf8bca57d
mohammadtuhin0 <md.tuhin42525@gmail.com>
2026-03-14 15:34:29 +0600

    Inheritance super function

---
+++ OOP/inheritance_super_function.py
@@ -0,0 +1,21 @@
+class Car:
+    def __init__(self, type):
+        self.type = type
+        
+    @staticmethod
+    def start():
+        print("Car started..")
+        
+    @staticmethod
+    def stop():
+        print("Car stopped.")
+        
+class ToyotaCar(Car):
+    def __init__(self, name, type):
+        super().__init__(type)
+        self.name = name
+        super().start()
+        
+        
+car1 = ToyotaCar("Prius", "Electric")
+print(car1.type)
